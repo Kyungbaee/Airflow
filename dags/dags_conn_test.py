4da972d32fd8ba19858eb167127654377dc93fab
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-06-09 23:52:26 +0900

    테스트 dags 수정

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -5,7 +5,7 @@ from airflow.operators.empty import EmptyOperator
 
 with DAG(
     dag_id="dags_conn_test",
-    schedule="0 0 * * *",
+    schedule=None,
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
@@ -35,5 +35,5 @@ with DAG(
     )
 
     t1 >> [t2, t3] >> t4
-    t5 >> t5
+    t5 >> t4
     [t4, t7] >> t6 >> t8
