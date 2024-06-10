dd0b6c35979ccfc41eb66bce96a0aef713e8261a
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-06-10 16:15:28 +0900

    python operator 파일 수정

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -6,7 +6,7 @@ import random
 
 with DAG(
     dag_id="dags_python_operator",
-    schedule="30 60 * * *",
+    schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
