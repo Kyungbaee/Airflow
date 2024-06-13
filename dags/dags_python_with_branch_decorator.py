534704b8173839b6817b190a65b89bd75fd603de
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-06-13 16:28:20 +0900

    dag_id 변경

--- dags/dags_python_with_branch_decorator.py
+++ dags/dags_python_with_branch_decorator.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_branch_python_operator",
+    dag_id="dags_python_with_branch_decorator",
     schedule=None,
     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
     catchup=False
